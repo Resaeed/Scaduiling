5f990f412d44f2ee3869d62119e76e83b5577bc7
Resaeed <47356458+Resaeed@users.noreply.github.com>
2019-05-01 14:11:18 +0200

    Add files via upload

--- escaduling.java
+++ escaduling.java
@@ -17,28 +17,67 @@ public class escaduling
    }  
       public static class process_structure
       {
-          public static int process_id, arrival_time, burst_time, priority;
-          public static int q , ready ;
+          private  int process_id, arrival_time, burst_time, priority;
+          private int q , ready ;
+
+        public int getProcess_id() {
+            return process_id;
+        }
+
+     
+        public void setProcess_id(int process_id) {
+            this.process_id = process_id;
+        }
           
-          public  static void proces_id(int x)
-          {
-              process_id = x ;
-          }
-          
-           public static void brst_time(int x)
-          {
-              burst_time = x ;
-          }
-           
-            public static void arival_time(int x)
-          {
-             arrival_time = x ;
-          }
-            
-             public static void priorty(int x)
-          {
-              priority = x ;
-          }
+        public int getArrival_time() {
+            return arrival_time;
+        }
+
+       
+        public void setArrival_time(int arrival_time) {
+            this.arrival_time = arrival_time;
+        }
+
+        
+        public int getBurst_time() {
+            return burst_time;
+        }
+
+       
+        public void setBurst_time(int burst_time) {
+            this.burst_time = burst_time;
+        }
+
+       
+        public int getPriority() {
+            return priority;
+        }
+
+        
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+
+        
+        public int getQ() {
+            return q;
+        }
+
+      
+        public void setQ(int q) {
+            this.q = q;
+        }
+
+        
+        public int getReady() {
+            return ready;
+        }
+
+        
+        public void setReady(int ready) {
+            this.ready = ready;
+        }
+         
       }
     
     public static void main(String args[])
@@ -53,33 +92,33 @@ public class escaduling
         {
             System.out.println("Process ID : ");
             int x = scan.nextInt();
-            process[count].proces_id(x);
+            process[count].setProcess_id(x);
             
             System.out.println("Arrival_time : ");
             int t = scan.nextInt();
-            process[count].arival_time(t);
+            process[count].setArrival_time(t);
             
             System.out.println("Burst_time: ");
             int z = scan.nextInt();
-            process[count].brst_time(z);
+            process[count].setBurst_time(t);
             
             System.out.println("Process Priority : ");
             int r = scan.nextInt();
-            process[count].priorty(r) ;
+            process[count].setPriority(r) ;
             
-            temp_process = process[count].priority; 
-            process[count].q = Queue(temp_process);
-            process[count].ready = 0; 
+            temp_process = process[count].getPriority(); 
+            process[count].setQ(Queue(temp_process));
+            process[count].setReady(0); 
         }
         
-        time =  process[0].burst_time;
+        time =  process[0].getBurst_time();
         for(y=0;y<limit;y++)
         {
             for(count =y;count<limit;count++)
             {
-                if(process[count].arrival_time < time) 
+                if(process[count].getArrival_time() < time) 
                   {
-                        process[count].ready = 1; 
+                      process[count].setReady(1); 
                   }
             }
             
@@ -87,9 +126,9 @@ public class escaduling
             {
                 for(j = count + 1 ; j < limit ;j++)
                 {
-                     if(process[count].ready == 1 && process[j].ready == 1) 
+                     if(process[count].getReady() == 1 && process[j].getReady() == 1) 
                         { 
-                              if(process[count].q == 2 && process[j].q == 1) 
+                              if(process[count].getQ() == 2 && process[j].getQ() == 1) 
                               { 
                                     temp = process[count]; 
                                     process[count] = process[j]; 
@@ -103,11 +142,11 @@ public class escaduling
             { 
                   for(j = count + 1; j < limit; j++) 
                   {
-                        if(process[count].ready == 1 && process[j].ready == 1) 
+                        if(process[count].getReady() == 1 && process[j].getReady() == 1) 
                         { 
-                              if(process[count].q == 1 && process[j].q == 1) 
+                              if(process[count].getQ() == 1 && process[j].getQ() == 1) 
                               { 
-                                    if(process[count].burst_time > process[j].burst_time) 
+                                    if(process[count].getBurst_time() > process[j].getBurst_time()) 
                                     { 
                                           temp = process[count]; 
                                           process[count] = process[j]; 
@@ -122,15 +161,15 @@ public class escaduling
                   } 
             }
             
-            System.out.print("Process[] :   " + process[y].process_id + "     ");
-            System.out.print("Time :   " + time + " To " + time + process[y].burst_time);
+            System.out.print("Process[] :   " + process[y].getProcess_id() + "     ");
+            System.out.print("Time :   " + time + " To " + time + process[y].getBurst_time());
             System.out.println("");
-            time = time + process[y].burst_time; 
+            time = time + process[y].getBurst_time(); 
             for(count = y; count < limit; count++) 
             { 
-                  if(process[count].ready == 1) 
+                  if(process[count].getReady() == 1) 
                   { 
-                        process[count].ready = 0; 
+                      process[count].setReady(0); 
                   } 
             } 
         }
